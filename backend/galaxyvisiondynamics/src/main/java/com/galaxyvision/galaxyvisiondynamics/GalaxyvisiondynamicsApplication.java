beb3cd84096946e105e8eb966b003f6432eb33d3
Tijojsp <tijojsp.may@gmail.com>
2025-02-16 19:01:30 -0500

    Initialized the spring boot based code set up for backend

---
+++ backend/galaxyvisiondynamics/src/main/java/com/galaxyvision/galaxyvisiondynamics/GalaxyvisiondynamicsApplication.java
@@ -0,0 +1,13 @@
+package com.galaxyvision.galaxyvisiondynamics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GalaxyvisiondynamicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GalaxyvisiondynamicsApplication.class, args);
+	}
+
+}
---
+++ backend/galaxyvisiondynamics/src/test/java/com/galaxyvision/galaxyvisiondynamics/GalaxyvisiondynamicsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.galaxyvision.galaxyvisiondynamics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GalaxyvisiondynamicsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
